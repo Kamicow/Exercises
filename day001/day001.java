fdb3ffc5695d8caba7b8be007c20348250a50d3d
Talha Kır <122121936+talhakir@users.noreply.github.com>
2023-01-08 00:28:33 +0300

    Create day001.java

---
+++ day001/day001.java
@@ -0,0 +1,9 @@
+public class Java_deneme
+{
+  public static void main(String[] args) {
+    java.util.Scanner kb = new java.util.Scanner(System.in);
+    String x;
+	x = kb.nextLine();
+    System.out.println(x);
+  }
+}
