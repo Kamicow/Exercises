a484918b641f1dd31b0ed651ca3bb1929c155642
Talha Kır <122121936+talhakir@users.noreply.github.com>
2023-01-08 00:30:35 +0300

    Create day001.cs

---
+++ day001/day001.cs
@@ -0,0 +1,11 @@
+using System;
+
+  class Deneme 
+{
+    static void Main() 
+    {
+      Console.Write("Bir şey yazınız ");
+      string x = Console.ReadLine();
+      Console.Write(x);
+    }
+}
