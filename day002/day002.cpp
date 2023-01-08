220ef6793fb82a06b3392a158049eb0b6c30655e
Talha Kır <122121936+talhakir@users.noreply.github.com>
2023-01-08 15:01:58 +0300

    Create day002.cpp

---
+++ day002/day002.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+	int main() {
+		int x, y;
+		cout << "Bir sayi giriniz: ";
+		cin >> x;
+		cout << "Bir sayi daha giriniz: ";
+		cin >> y;
+		cout << "Girdiginiz sayilarin toplami: " << x+y;
+	}
