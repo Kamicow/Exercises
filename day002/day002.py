d668027034ca1d59fcfb8f25328adf8468472c21
Talha Kır <122121936+talhakir@users.noreply.github.com>
2023-01-08 15:02:33 +0300

    Update day002.py

--- day002/day002.py
+++ day002/day002.py
@@ -1,3 +1,10 @@
-x = int(input("Bir sayı giriniz: "))
-y = int(input("Bir sayı daha giriniz: "))
-print(f"Girdiğiniz sayıların toplamı: {x + y}")
+while True:
+    try:
+        x = int(input("Bir sayı giriniz: "))
+        y = int(input("Bir sayı daha giriniz: "))
+        print("İşlem başarılı, girdiğiniz sayıların toplamı:", x+y)
+        break
+
+    except Exception:
+        print("Lütfen tam sayı değeri giriniz!")
+        continue
