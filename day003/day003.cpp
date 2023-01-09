fe139f34f99939dd35f753b2faccd9e0b5a597bb
Talha Kır <122121936+talhakir@users.noreply.github.com>
2023-01-09 16:50:35 +0300

    Create day003.cpp

---
+++ day003/day003.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+	int main(){
+		int x, y;
+		cout << "Dikdörtgenin kısa kenarını giriniz: ";
+		cin >> x;
+		cout << "Dikdörtgenin uzun kenarını giriniz: ";
+		cin >> y;
+		cout << "Ölçülerini girdiğiniz dikdörtgenin alanı: " << x*y;
+}
