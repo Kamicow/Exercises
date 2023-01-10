7c73d1285415ecca9dff5da00544f6f892aaf2db
Talha Kır <122121936+talhakir@users.noreply.github.com>
2023-01-10 17:12:27 +0300

    Create day004-1.cpp

---
+++ day004/day004-1.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+	int main() {
+	float r, pi;
+	pi = 3.14;
+	cout << "Dairenin yaricapini giriniz";
+	cin >> r;
+	cout << "Dairenin alani:" << r*r*pi;
+}
