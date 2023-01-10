63f788eb5fa352aec4c734cd3248cf7792758d27
Talha Kır <122121936+talhakir@users.noreply.github.com>
2023-01-10 17:31:22 +0300

    Create day004-2.cs

---
+++ day004/day004-2.cs
@@ -0,0 +1,13 @@
+using System;
+
+	class Day004 {
+		static void Main() {
+			double r, h, pi;
+			pi = 3.14;
+			Console.Write("Dairenin capini giriniz:");
+			r = Convert.ToInt32(Console.ReadLine());
+			Console.Write("Silindirin yuksekligini giriniz:");
+			r = Convert.ToInt32(Console.ReadLine());
+			Console.Write("Girdiginiz silindirin hacmi:" + r*r*h*pi); 
+		}
+}
