0f2bec313465df9991e9025fa58e838dd8fd5b6a
Talha Kır <122121936+talhakir@users.noreply.github.com>
2023-01-10 17:09:31 +0300

    Create day004-1.c

---
+++ day004/day004-1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+	int r; 
+	double pi;
+	pi = 3.14;
+	printf("Dairenin capini giriniz");
+	scanf("%d", &r);
+	printf("Olculerini girdiginiz dairenin: %f", (r*r*pi));
+	
+}
