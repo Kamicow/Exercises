774c1e581cc2340a52f4f932ef05b12fb75a90a6
Talha Kır <122121936+talhakir@users.noreply.github.com>
2023-01-10 17:30:20 +0300

    Create day004-2.cpp

---
+++ day004/day004-2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+	int main() {
+	float r, h, pi;
+	pi = 3.14;
+	cout << "Silindirin yaricapini giriniz";
+	cin >> r;
+	cout << "Silindirin yuksekligini giriniz";
+	cin << h;
+	cout << "Silindirin hacmi:" << r*r*h*pi;
+}
