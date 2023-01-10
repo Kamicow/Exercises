dd0cd6184ae8b85821382226731843178560fbcf
Talha Kır <122121936+talhakir@users.noreply.github.com>
2023-01-10 17:33:46 +0300

    Update day004-2.py

--- day004/day004-2.py
+++ day004/day004-2.py
@@ -1,6 +1,6 @@
-print(int(input("Silindirin yaricapini yaziniz: ")) * int(input("Silindirin yuksekliğini giriniz: ")) * 3.14)
+print((int(input("Silindirin yaricapini yaziniz: ")) ** 2) * int(input("Silindirin yuksekliğini giriniz: ")) * 3.14)
 
 ----------
 
 import math
-print(int(input("Silindirin yaricapini yaziniz: ")) * int(input("Silindirin yuksekliğini giriniz: ")) * math.pi)
+print((int(input("Silindirin yaricapini yaziniz: ")) ** 2) int(input("Silindirin yuksekliğini giriniz: ")) * math.pi)
