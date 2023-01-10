699a996179dc135ab33f461336d32a21f8a6ddbb
Talha Kır <122121936+talhakir@users.noreply.github.com>
2023-01-10 17:11:59 +0300

    Create day004-1.java

---
+++ day004/day004-1.java
@@ -0,0 +1,10 @@
+public class Day004java {
+	public static void main(String[] args) {
+		java.util.Scanner kb = new java.util.Scanner(System.in);
+		int r;
+		System.out.println("Dairenin yari capini giriniz:");
+		r = kb.nextInt();
+		System.out.println("Dairenin alani:" + Math.PI*r*r);
+	}
+}
+
