2dc0afe87e70c563674ff51bf71e704f8d68a308
Talha Kır <122121936+talhakir@users.noreply.github.com>
2023-01-11 18:36:41 +0300

    Create day005.py

---
+++ day005/day005.py
@@ -0,0 +1,8 @@
+x = input("İstediğiniz bir şeyi giriniz: ")
+y = input("Bir şey daha giriniz: ")
+
+temp = x
+x = y
+y = temp
+
+print("Girdiğiniz değerlerin yeri değişti. İlk değer:", x, " İkinci değer:", y)
