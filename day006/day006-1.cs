3a5554611c65d30e8c2f5393341c99d32bcf500e
Talha Kır <122121936+talhakir@users.noreply.github.com>
2023-01-12 16:14:58 +0300

    Update day006-1.cs

--- day006/day006-1.cs
+++ day006/day006-1.cs
@@ -3,10 +3,13 @@ using System;
 	class Day006 {
 		static void Main() {
 			float x, y;
+			
 			Console.Write("Bir sayı giriniz: X ");
 			x = Convert.ToInt32(Console.ReadLine());
+			
 			Console.Write("Başka bir sayı daha giriniz: Y ");
 			y = Convert.ToInt32(Console.ReadLine());
+			
 			Console.WriteLine(x+y); 
 			Console.WriteLine(x-y);
 			Console.WriteLine(x*y);
