b0a0b513348a80110fb9a4ae94c0577ce6d8bd41
Talha Kır <122121936+talhakir@users.noreply.github.com>
2023-01-12 16:13:14 +0300

    Update day006-2.java

--- day006/day006-2.java
+++ day006/day006-2.java
@@ -2,11 +2,15 @@ public class Main {
 	
 	public static void main(String[] args) {
 		java.util.Scanner kb = new java.util.Scanner(System.in);
+		
 		float x, y;
+		
 		System.out.print("Bir sayı giriniz: X ");
 		x = kb.nextFloat();
+		
 		System.out.print("Başka bir sayı daha giriniz: Y ");
 		y = kb.nextFloat();
+		
 		System.out.println(x+y);
 		System.out.println(x-y);
 		System.out.println(x*y);
