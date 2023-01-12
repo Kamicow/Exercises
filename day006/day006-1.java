878dbe5c0cacc4163521151254db8a61da13c693
Talha Kır <122121936+talhakir@users.noreply.github.com>
2023-01-12 16:11:22 +0300

    Create day006-1.java

---
+++ day006/day006-1.java
@@ -0,0 +1,16 @@
+public class Day006 {
+	
+	public static void main(String[] args) {
+		java.util.Scanner kb = new java.util.Scanner(System.in);
+		int x, y;
+		System.out.print("Bir sayı giriniz: X ");
+		x = kb.nextInt();
+		System.out.print("Başka bir sayı daha giriniz: Y ");
+		y = kb.nextInt();
+		System.out.println(x+y);
+		System.out.println(x-y);
+		System.out.println(x*y);
+		System.out.println(x/y);
+	}
+}
+
