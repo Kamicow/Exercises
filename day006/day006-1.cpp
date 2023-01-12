b9a667383ca332b0ae99447f73b49b9bdd92e291
Talha Kır <122121936+talhakir@users.noreply.github.com>
2023-01-12 16:10:48 +0300

    Create day006-1.cpp

---
+++ day006/day006-1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+	int main() {
+		int x, y;
+		cout << "Bir sayı giriniz ";
+		cin >> x;
+		cout << "Bir sayı giriniz ";
+		cin >> y;
+		cout << x+y << "\n";
+		cout << x-y << "\n";
+		cout << x*y << "\n";
+		cout << x/y << "\n";
+	}
