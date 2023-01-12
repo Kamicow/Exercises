454e863218ceec239700fd33d3e1c8a405fbe0a6
Talha Kır <122121936+talhakir@users.noreply.github.com>
2023-01-12 16:10:11 +0300

    Create day006-1.c

---
+++ day006/day006-1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+	int x, y;
+	printf("Bir sayı giriniz ");
+	scanf("%d", &x);
+	printf("Bir sayı giriniz ");
+	scanf("%d", &y);
+	printf("%d \n", (x+y));
+	printf("%d \n", (x-y));
+	printf("%d \n", (x*y));
+	printf("%d \n", (x/y));
+	return 0;
+}
