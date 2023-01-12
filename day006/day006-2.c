ef9369c3ee196180de56180da2ec057f30f1322e
Talha Kır <122121936+talhakir@users.noreply.github.com>
2023-01-12 16:14:10 +0300

    Update day006-2.c

--- day006/day006-2.c
+++ day006/day006-2.c
@@ -2,13 +2,17 @@
 
 int main() {
 	float x, y;
+	
 	printf("Bir sayı giriniz ");
 	scanf("%f", &x);
+	
 	printf("Bir sayı giriniz ");
 	scanf("%f", &y);
+	
 	printf("%f \n", (x+y));
 	printf("%f \n", (x-y));
 	printf("%f \n", (x*y));
 	printf("%f \n", (x/y));
+	
 	return 0;
 }
