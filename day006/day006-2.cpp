09b2f4446f66ea688ffb1182dbccfb8d3676d0a8
Talha Kır <122121936+talhakir@users.noreply.github.com>
2023-01-12 16:12:10 +0300

    Create day006-2.cpp

---
+++ day006/day006-2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+	int main() {
+		float x, y;
+		cout << "Bir sayı giriniz ";
+		cin >> x;
+		cout << "Bir sayı giriniz ";
+		cin >> y;
+		cout << (float)x + (float)y << "\n";
+		cout << (float)x - (float)y << "\n";
+		cout << (float)x * (float)y << "\n";
+		cout << (float)x / (float)y << "\n";
+	}
