5612e856304a5cddf7bfaff3fa65dc4096238d7d
Talha Kır <122121936+talhakir@users.noreply.github.com>
2023-01-13 22:31:29 +0300

    Update day007.c

--- day007/day007.c
+++ day007/day007.c
@@ -2,12 +2,15 @@
 
 int main()
 {
-    int x, sum;
-    sum = 0;
+    int x, sum = 0;
+    
     printf("Girdiğiniz sayıların rakamlarının karelerinin toplamı size söylenecek. \nBir sayı giriniz");
     scanf("%d", &x);
+    
     sum += (x % 10) * (x % 10); 
     sum += (x / 10) * (x / 10); 
+    
     printf("%d", sum);
+    
     return 0;
 }
