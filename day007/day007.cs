a8dd8c49b75f880b93083183b330c19006f72932
Talha Kır <122121936+talhakir@users.noreply.github.com>
2023-01-13 22:29:54 +0300

    Create day007.cs

---
+++ day007/day007.cs
@@ -0,0 +1,11 @@
+using System;
+  class Day007 {
+    static void Main() {
+      int x, sum = 0;
+      Console.Write("Bir sayı giriniz: ");
+      x = Convert.ToInt32(Console.ReadLine());
+      sum += (x % 10) * (x % 10); 
+      sum += (x / 10) * (x / 10);
+      Console.WriteLine(sum);
+    }
+  }
