0d4accacf45236d92ce8a9f8d764cad455bf05d2
Talha Kır <122121936+talhakir@users.noreply.github.com>
2023-01-13 22:31:03 +0300

    Update day007.cpp

--- day007/day007.cpp
+++ day007/day007.cpp
@@ -8,8 +8,11 @@ int main()
     
     cout<<"Bir sayı giriniz. Bu sayının rakamlarının karesi size söylenecektir. ";
     cin >> x;
+   
     sum += (x % 10) * (x % 10); 
     sum += (x / 10) * (x / 10);
+    
     cout << sum;
+    
     return 0;
 }
