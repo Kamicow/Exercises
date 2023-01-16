b44322e2e90abc62917dda1e03c2cce178a5b737
Talha Kır <122121936+talhakir@users.noreply.github.com>
2023-01-16 22:42:54 +0300

    Create day010.py

---
+++ day010/day010.py
@@ -0,0 +1,10 @@
+x = int(input("Bir sayı giriniz: "))
+
+if x != 0 and x < 0:
+    print("Girdiğiniz sayı negatiftir.")
+    
+elif x != 0 and x > 0:
+    print("Girdiğiniz sayı pozitiftir.")
+
+else:
+    print("Girdiğiniz sayı nötr bir sayıdır.")
