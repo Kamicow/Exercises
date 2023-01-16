cd4f4f225e1d25da2159ba103677081751484348
Talha Kır <122121936+talhakir@users.noreply.github.com>
2023-01-16 22:43:17 +0300

    Create day010.java

---
+++ day010/day010.java
@@ -0,0 +1,20 @@
+public class Main
+{
+	public static void main(String[] args) {
+		java.util.Scanner kb = new java.util.Scanner(System.in);
+		int x;
+		
+		System.out.print("Bir sayı giriniz: ");
+		x = kb.nextInt();
+		
+		if (x != 0 && x < 0)
+		    System.out.println("Girdiğiniz sayı negatiftir.");
+		    
+		else if (x != 0 && x > 0)
+		    System.out.println("Girdiğiniz sayı pozitiftir.");
+		
+		else 
+		    System.out.println("Girdiğiniz sayı nötrdür.");
+		
+	}
+}
