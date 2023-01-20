6ae865060ef7bbff43065ad594e505ca61cfdd6e
Talha Kır <122121936+talhakir@users.noreply.github.com>
2023-01-20 19:38:43 +0300

    Create day013.cpp

---
+++ day013/day013.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int x, y;
+    
+    cout << "Bir sayı giriniz: ";
+    cin >> x;
+    cout << "Başka bir sayı giriniz: ";
+    cin >> y;
+    
+    if (x < y)
+        cout << y << " büyüktür " << x;    
+    else if (x > y)
+        cout << x << " büyüktür " << y;       
+    else
+        cout << "Girdiğiniz sayılar eşittir.";
+
+    return 0;
+}
