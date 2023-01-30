0ae9d645bcd5ab0ab9a1f31aee1095ec72c9cb78
Talha Kır <122121936+talhakir@users.noreply.github.com>
2023-01-30 16:05:51 +0300

    Create day016.java

---
+++ day016/day016.java
@@ -0,0 +1,20 @@
+public class Day016
+{
+	public static void main(String[] args) {
+		java.util.Scanner kb = new java.util.Scanner(System.in);
+		int x, i, temp = 0;
+        
+        System.out.print("Girdiğiniz sayının çarpanları gösterilecektir: ");
+        x = kb.nextInt();
+    
+        for (i = 1; i < x + 1; i++) {
+            if (x % i == 0) {
+                temp = i;
+                if (x / i < temp)
+                break;
+                System.out.print(i + " ");
+                System.out.println(x/i);
+            }
+        }
+	}
+}
