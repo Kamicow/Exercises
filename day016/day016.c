86958068eeb5ecf38cdec2553b8e818f434907cc
Talha Kır <122121936+talhakir@users.noreply.github.com>
2023-01-30 16:05:34 +0300

    Create day016.c

---
+++ day016/day016.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+    int x, i, temp = 0;
+    printf("Girdiğiniz sayının çarpanları gösterilecektir: ");
+    scanf("%d", &x);
+    
+    for (i = 1; i < x + 1; i++) {
+        if (x % i == 0) {
+            temp = i;
+            if (x / i < temp)
+            break;
+            printf("%d %d \n", i, x/i);
+        }
+    }
+
+    return 0;
+}
